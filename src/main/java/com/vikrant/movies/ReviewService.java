bb71f429a3926ad498280fe21cf02773687f984e
vickky122 <vikrantmani45@gmail.com>
2024-03-08 20:58:59 +0530

    commit

--- src/main/java/com/vikrant/movies/ReviewService.java
+++ src/main/java/com/vikrant/movies/ReviewService.java
@@ -3,7 +3,7 @@ package com.vikrant.movies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Update;
+//import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -20,9 +20,9 @@ public class ReviewService {
         Review review = repository.insert(new Review(reviewBody, LocalDateTime.now(), LocalDateTime.now()));
 
         mongoTemplate.update(Movie.class)
-                .matching(Criteria.where("imdbId").is(imdbId))
-                .apply(new Update().push("reviewIds").value(review.getId()))
-                .first();
+                .matching(Criteria.where("imdbId").is(imdbId));
+        // .apply(new Update().push("reviewIds").value(review.getId()))
+        // .first();
         return review;
     }
 }
